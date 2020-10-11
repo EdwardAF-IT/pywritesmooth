7748f7fdb688ad85245f0a29bd3e589cc867935c
Edward Fry <magic.elf@outlook.com>
2020-10-11 16:17:34 -0500

    0.0.17

--- Code/HandwritingSmoother/pywritesmooth/TrainSmoother/__init__.py
+++ Code/HandwritingSmoother/pywritesmooth/TrainSmoother/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'YuMei Bennett, Edward Fry, Muchigi Kimari, Ikenna Nwaogu'
 __credits__ = 'Dr John Santerre'
-__all__ = ['GANTrainer', 'StyleTransferTrainer', 'RNNTrainer', 'AutoEncoderTrainer', 'SVDTrainer']
+__all__ = ['GANTrainer', 'StyleTransferTrainer', 'RNNTrainer', 'AutoEncoderTrainer', 'SVDTrainer', 'TrainerInterface']
 
 from . import *

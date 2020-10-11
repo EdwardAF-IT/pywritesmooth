fd330be7906b5339cb1d3a373fe2f5453302c56b
Edward Fry <magic.elf@outlook.com>
2020-10-11 17:02:55 -0500

    0.0.23

--- Code/HandwritingSmoother/setup.py
+++ Code/HandwritingSmoother/setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
     python_requires='>=3.7',
     entry_points={
     'console_scripts': [
-        'pywritesmooth = pws.HandwritingSmoother:main',
+        'pywritesmooth = HandwritingSmoother:main',
     ],
 },
 )

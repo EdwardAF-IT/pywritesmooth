855c6f1e8d676da5acecca89ca3f2969015f8d32
Edward Fry <magic.elf@outlook.com>
2021-01-24 17:06:21 -0600

    0.1.4

--- Code/HandwritingSmoother/pywritesmooth/__init__.py
+++ Code/HandwritingSmoother/pywritesmooth/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = 'YuMei Bennett, Edward Fry, Muchigi Kimari, Ikenna Nwaogu'
 __credits__ = 'Dr John Santerre'
 __all__ = ['HandwritingSmoother']
